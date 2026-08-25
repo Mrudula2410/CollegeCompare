05bf759ce6756a7e6708410a360767d94e05e83d
Mrudula2410 <mrudulaparate24@gmail.com>
2026-08-25 17:35:35 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,72 +1,19 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
 
-const collegeRoutes = require("./routes/collegeRoutes");
-const userRoutes = require("./routes/userRoutes");
-const savedCollegeRoutes= require("./routes/savedCollegeRoutes");
 const app = express();
 
-
-// ===============================
-// MIDDLEWARE
-// ===============================
-
-app.use(cors());
+const PORT = 3001;
 
 app.use(express.json());
 
-
-// ===============================
-// MONGODB
-// ===============================
-
-mongoose.connect(
-    "mongodb://127.0.0.1:27017/collegeCompare"
-)
-.then(() => {
-
-    console.log("MongoDB connected");
-
-})
-.catch((error) => {
-
-    console.log(
-        "MongoDB connection error:",
-        error
-    );
-
+app.get("/", (req, res) => {
+    res.send("Express Server is Working!");
 });
 
-
-// ===============================
-// ROUTES
-// ===============================
-
-app.use(
-    "/api/colleges",
-    collegeRoutes
-);
-
-app.use(
-    "/api/users",
-    userRoutes
-);
-
-app.use(
-    "/api/saved-colleges",
-    savedCollegeRoutes
-);
-// ===============================
-// SERVER
-// ===============================
-
-const PORT = 5000;
+app.get("/about", (req, res) => {
+    res.send("This is About Page");
+});
 
 app.listen(PORT, () => {
-
-    console.log(
-        `Server running on http://localhost:${PORT}`
-    );
-
+    console.log(`Express server running on http://localhost:${PORT}`);
 });
